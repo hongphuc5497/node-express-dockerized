9e08eaefcdc08f9cd0952af27b3321dcd147c3fb
Hong Phuc <hongphuc.dthp@gmail.com>
2022-01-27 12:06:39 +0700

    setup server, add logger, update gitignore

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 
+const logger = require('./config/winston');
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.status(200).send('Hello World!');
+});
+
+const port = 4000;
+const host = 'localhost';
+
+app.listen(port, () => {
+	logger.info(`Server started and running on http://${host}:${port}`);
+});
